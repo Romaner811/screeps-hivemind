5ecd5b1a7452c1f2f7fb3064c49c3022c061afa7
Roman Guntik <romaner811@gmail.com>
2024-06-19 00:35:09 +0300

    jsdoc + add pos_parse to use in future to store pos in memory

--- src/util.cjs
+++ src/util.cjs
@@ -1,6 +1,10 @@
 /** @module */
 
 
+/**
+ * @class
+ * @extends Error
+ */
 class NotImplementedError extends Error
 {
     constructor()
@@ -9,18 +13,47 @@ class NotImplementedError extends Error
     }
 }
 
-
-function pos_describe(pos)
+/**
+ * 
+ * @param {RoomPosition} pos 
+ * @returns {string} 
+ */
+function pos_stringify(pos)
 {
-    return `${pos.room.name}(${pos.x},${pos.y})`;
+    return `${pos.roomName}(${pos.x},${pos.y})`;
+}
+
+const RE_POS = /(?<room>[\w]+)\((?<x>-?\d),(?<y>-?\d)\)/;
+/**
+ * 
+ * @param {string} txt_pos 
+ * @returns {RoomPosition}
+ */
+function pos_parse(txt_pos)
+{
+    let pos_match = txt_pos.match(RE_POS);
+    if (pos_match == null) return null;
+
+    return new RoomPosition(pos_match["x"], pos_match["y"], pos_match["room"]);
 }
 
 
+/**
+ * 
+ * @param {Array} arr 
+ * @param {number} idx 
+ */
 function arr_remove_at(arr, idx)
 {
     arr.splice(idx, 1);
 }
-    
+
+/**
+ * 
+ * @template T
+ * @param {Array<T>} arr 
+ * @param {T} item
+ */
 function arr_remove(arr, item)
 {
     const idx = arr.indexOf(item);
@@ -30,6 +63,13 @@ function arr_remove(arr, item)
     return true;
 }
 
+/**
+ * 
+ * @template T
+ * @param {Array<T>} arr 
+ * @param {number} idx 
+ * @param {...T} items
+ */
 function arr_insert(arr, idx, ...items)
 {
     arr.splice(idx, 0, ...items);
@@ -39,7 +79,8 @@ function arr_insert(arr, idx, ...items)
 module.exports = {
     NotImplementedError,
     
-    pos_describe,
+    pos_stringify,
+    pos_parse,
 
     arr_remove_at,
     arr_remove,
